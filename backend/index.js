991ba41a52cf31a1e96584b9a9e19fdb5bb0915c
M-Roxana <manolacheroxana96@yahoo.com>
2023-02-18 10:42:10 +0200

    reports

--- backend/index.js
+++ backend/index.js
@@ -43,15 +43,74 @@ app.get("/categorii/:idCategorie", (req, res) => {
   });
 });
 
-app.get("/vanzari/:id_categorie/:start_date/:end_date", (req, res) => {
-  const idCategorie = req.params.id_categorie;
-  const startDate = req.params.start_date;
-  const endDate = req.params.end_date;
+app.get("/vanzari", (req, res) => {
+  const { categorie, start_date, end_date } = req.query;
   const q =
-    "SELECT * FROM vanzari INNER JOIN produse ON produse.id_produs = vanzari.id_produs WHERE id_categorie = ? and vanzari.data >= ? and vanzari.date <= ?";
-  db.query(q, [idCategorie, startDate, endDate], (err, data) => {
+    "SELECT * FROM vanzari INNER JOIN produse ON produse.id_produs = vanzari.id_produs WHERE id_categorie = ? and data >= ? and data <= ?";
+  db.query(q, [categorie, start_date, end_date], (err, data) => {
     if (err) return res.json(err);
     return res.json(data);
+  });
+});
+
+app.get("/raport-produse", (req, res) => {
+  const { produs, start_date, end_date } = req.query;
+  const q =
+    "SELECT * FROM vanzari WHERE id_produs = ? and data >= ? and data <= ?";
+  db.query(q, [produs, start_date, end_date], (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.get("/raport/:year", (req, res) => {
+  const year = req.params.year;
+  const q = `
+    SELECT 
+      YEAR(data) AS an, 
+      MONTH(data) AS luna, 
+      produse.nume_produs AS produs, 
+      categorii.nume_categorie AS categorie, 
+      SUM(vanzari.cantitate) AS cantitate_vanduta 
+    FROM vanzari 
+      INNER JOIN produse ON produse.id_produs = vanzari.id_produs 
+      INNER JOIN categorii ON categorii.id_categorie = produse.id_categorie 
+    WHERE YEAR(data) = ? 
+    GROUP BY YEAR(data), MONTH(data), produse.id_produs 
+    ORDER BY YEAR(data), MONTH(data), cantitate_vanduta DESC 
+  `;
+  db.query(q, [year], (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+    // const results = {};
+    // for (const row of data) {
+    //   const monthKey = `${row.an}-${row.luna}`;
+    //   if (!results[monthKey]) {
+    //     results[monthKey] = {
+    //       luna: row.luna,
+    //       an: row.an,
+    //       categorie_ceruta: {
+    //         denumire: row.categorie,
+    //         cantitate_vanduta: row.cantitate_vanduta,
+    //       },
+    //       produs_vandut: {
+    //         denumire: row.produs,
+    //         cantitate_vanduta: row.cantitate_vanduta,
+    //       },
+    //     };
+    //   } else {
+    //     if (row.cantitate_vanduta > results[monthKey].produs_vandut.cantitate_vanduta) {
+    //       results[monthKey].produs_vandut.denumire = row.produs;
+    //       results[monthKey].produs_vandut.cantitate_vanduta = row.cantitate_vanduta;
+    //     }
+    //     if (row.cantitate_vanduta > results[monthKey].categorie_ceruta.cantitate_vanduta) {
+    //       results[monthKey].categorie_ceruta.denumire = row.categorie;
+    //       results[monthKey].categorie_ceruta.cantitate_vanduta = row.cantitate_vanduta;
+    //     }
+    //   }
+    // }
+    // const finalResult = Object.values(results);
+    //return res.json(finalResult);
   });
 });
 
